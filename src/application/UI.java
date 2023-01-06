f9ccc8bbaf4afcb20d7f9415348bb5d6748fe681
Marco Domingues <marco.a.dom78@gmail.com>
2023-01-06 23:44:20 +0000

    Check logic implemented

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,9 @@ public class UI {
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Wanting player: " + chessMatch.getCurrentPlayer());
+        if (chessMatch.getCheck()) {
+            System.out.println("CHECK!");
+        }
 
     }
 
@@ -99,7 +102,6 @@ public class UI {
         }
         System.out.print(" ");
     }
-
 
 
     private static void printCapturedPieces(List<ChessPiece> captured) {
